da54beed4a3acb4f28217d58ca137fbfceae040b
Yasir Fayrooz <30676134+yasir-fayrooz@users.noreply.github.com>
2020-09-12 08:43:11 +1000

    added initial models for the DB

---
+++ miniCarsales/miniCarsales/Models/E_BodyType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace carsales.Models
+{
+    public enum BodyType
+    {
+        Hatchback,
+        Sedan,
+        SUV,
+        Ute,
+        Coupe,
+        Convertible,
+        Wagon,
+        Van,
+    }
+}
---
+++ miniCarsales/miniCarsales/Models/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace carsales.Models
+{
+    public abstract class Vehicle
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public VehicleType VehicleType { get; set; }
+    }
+}
