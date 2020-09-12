410b51bc5fc0603661c753438a0b37992e143fd0
Yasir Fayrooz <30676134+yasir-fayrooz@users.noreply.github.com>
2020-09-12 11:49:46 +1000

    added home page, made some decent responsive code and validation

--- miniCarsales/miniCarsales/ClientApp/src/app/home/home.component.ts
+++ miniCarsales/miniCarsales/ClientApp/src/app/home/home.component.ts
@@ -3,6 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.css']
 })
 export class HomeComponent {
+  private validSelection: boolean = false;
+  private otherSelected: boolean = false;
+
+  public OnOptionSelected(selectedValue: string): void {
+    if (selectedValue == 'Other') {
+      this.validSelection = false;
+      this.otherSelected = true;
+    }
+    else {
+      this.otherSelected = false;
+    }
+
+    if (selectedValue == 'Car') {
+      this.validSelection = true;
+    }
+  }
 }
